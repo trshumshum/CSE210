1e344151a2e4ff760eaac0ae10aeb368e8869fcb
trshumshum <john@shumwayfamily.net>
2025-01-10 15:51:33 -0700

    modified hello world to be James Bond Thingy

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,21 @@
+// John Shumway - W01 Prove Prep1
 using System;
 
 class Program
+
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //get first name and last name from user
+        Console.WriteLine("What is your first name?");
+        string f_name = Console.ReadLine();
+        
+        Console.WriteLine("what is your last name?:");
+        string l_name = Console.ReadLine();
+
+        // print out the James Bond Thingy
+        Console.WriteLine($"Your name is {l_name}, {f_name} {l_name}.");
     }
 }
+
+
