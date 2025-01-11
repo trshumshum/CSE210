ba232a3cb7ef42e40abbea164dcb5bbe1ab16f86
trshumshum <john@shumwayfamily.net>
2025-01-10 18:53:27 -0700

    week 2 - Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,44 @@
+// John Shumway - W02 Prove Prep3
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Generate a random number between 1 and 100
+        Random random = new Random();
+        int numberToGuess = random.Next(1, 101); //changed to 101 after looking at sample solution
+
+        //Variables
+        int guessCount = 0; 
+        int guess;
+        bool keepGuessing = true;
+        
+        //Start guessing game
+        Console.WriteLine("The magic number is between 1 and 100.");
+        
+        
+        // Begin guessing loop
+        while(keepGuessing)
+        {
+            guessCount++; //increment 
+            Console.Write("Enter your guess: ");
+            guess = int.Parse(Console.ReadLine());
+            
+            if (guess == numberToGuess)
+            {
+                Console.WriteLine($"Congratulations! You've guessed it right! It took you {guessCount} attempts.");
+                break; //exit the loop and program
+            }
+            else if (guess < numberToGuess)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else
+            {
+                Console.WriteLine("Lower!");
+            }
+        }
+        
     }
 }
