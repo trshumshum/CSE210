75f0d33f47ee2fe718e8f56f3216e6793f4ce9f6
trshumshum <john@shumwayfamily.net>
2026-03-06 16:37:41 -0700

    prep 2 assignment upload

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("This is the Prep2 Assignment");
+
+         //get grade percentage from user
+        Console.Write("Please give me your grade percenage: ");
+        string GradeString = Console.ReadLine();
+
+        //convert GradeString to GradeInt
+        int GradeInt = int.Parse(GradeString);
+
+        //calculate the grade, if they passed and assign GradeLetter
+        string GradeLetter = "CODE OR INPUT ERROR";
+        bool Passed = GradeInt >= 70;
+        if (GradeInt >= 90) GradeLetter = "A";
+        else if (GradeInt >= 80) GradeLetter = "B";
+        else if (GradeInt >= 70) GradeLetter = "C";
+        else if (GradeInt >= 60) GradeLetter = "D";
+        else GradeLetter = "F";
+
+        //print out grade letter
+        Console.WriteLine($"Your Grade is: {GradeLetter} ");
+
+        if (Passed)
+            Console.Write("Congratulation you passed the Class!");
+        else
+            Console.Write("Sorry, you didn't pass. Keep trying.");
+
+
+
+
     }
 }
