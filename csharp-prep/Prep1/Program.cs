e3c73154a8f2a542e446ed1cb5508f573c791bbb
trshumshum <john@shumwayfamily.net>
2026-03-06 16:27:30 -0700

    upload Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //John S
+        Console.WriteLine("This is the Prep1 Assignment.");
+
+        //get first name 
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        //get last name 
+        Console.Write("What is your last name: ");
+        string last_name = Console.ReadLine();
+
+        //display name in james bond style :)
+        Console.WriteLine($"your name is {last_name}, {first_name} {last_name}.");
+
+        
     }
 }
