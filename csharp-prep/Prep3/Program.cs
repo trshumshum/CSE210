abd57713a2f0878e7a875a952bc2b75d4af2cbe1
trshumshum <john@shumwayfamily.net>
2026-03-06 16:47:46 -0700

    Prep3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Prep 3 Assignment by John S.");
+
+        Console.WriteLine("Welcome to the Guessing Game!");
+
+        // create a random number between 1 and 100
+        Random RandomMagicNumber = new Random();
+        int MagicNumber = RandomMagicNumber.Next(1, 101);
+
+        Console.WriteLine("Try and guess the magic number between 1 and 100:");
+        int guess = 0;
+
+        while (guess != MagicNumber)
+        {
+            string input = Console.ReadLine();
+            guess = int.Parse(input);
+
+            if (guess < MagicNumber) Console.WriteLine("Sorry try again.. Hint: Guess higher.");
+            else if (guess > MagicNumber) Console.WriteLine("Sorry try again.. Hint: Guess lower.");
+            else Console.WriteLine($"You got it! The magic number was: {MagicNumber}. ");
+        }
+        
+        Console.WriteLine("Thanks for playing! Goodbye.");
+
+
     }
 }
